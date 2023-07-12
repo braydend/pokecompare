3cee62c7d6a80ae75b1f5498ebd72d74487a7dd5
braydend <braydendiluca@gmail.com>
2023-07-12 10:04:44 +1000

    Add random pokemon API endpoint

--- main.go
+++ main.go
@@ -1,18 +1,48 @@
 package main
 
 import (
-	"fmt"
-	"log"
+	"encoding/json"
+	"net/http"
 
 	"github.com/braydend/pokecompare/api"
 )
 
+const PORT = ":8000"
+
+type RandomPokemonResponse struct {
+	Name   string
+	ID     int
+	Sprite string
+	Types  []string
+}
+
 func main() {
-	pokemon, err := api.GetRandomPokemon()
+	http.HandleFunc("/random-pokemon", func(w http.ResponseWriter, r *http.Request) {
+		pokemon, err := api.GetRandomPokemon()
+		if err != nil {
+			w.WriteHeader(500)
+			w.Write([]byte(err.Error()))
+		}
 
-	if err != nil {
-		log.Fatal(fmt.Sprintf("Failed to get a pokemon %s", err))
-	}
+		var types []string
 
-	fmt.Printf("Successfully got %s", pokemon.Name)
+		for _, pokemonType := range pokemon.Types {
+			types = append(types, pokemonType.Type.Name)
+		}
+
+		resp, err := json.Marshal(RandomPokemonResponse{
+			Name:   pokemon.Name,
+			ID:     pokemon.ID,
+			Sprite: pokemon.Sprites.FrontDefault,
+			Types:  types,
+		})
+
+		if err != nil {
+			w.WriteHeader(500)
+			w.Write([]byte(err.Error()))
+		}
+		w.Write(resp)
+	})
+
+	http.ListenAndServe(PORT, nil)
 }
